8b0807d10ad6678b708043adee96a4f9e2a7e9a9
deybyr647 <deybyr647@gmail.com>
2020-08-07 20:21:10 -0400

    Edits to the test user instance & the User class

--- recycle.js
+++ recycle.js
@@ -1,17 +1,17 @@
 let database = firebase.database().ref()
 
-let testUsername = 'deybyr647', testFirstName='Deyby', testLastName='Rodriguez', testEmail='deybyr647@gmail.com', testPassword='testPassword';
+let testUsername = 'deybyr647', testFirstName='Deyby', testLastName='Rodriguez', testEmail='deybyr647@gmail.com', testPassword='testPassword', testSavedArticles = ['medium', 'e'];
 
 
 let refreshDB = () => {
     //e.preventDefault();
-    const newUser = new User(testFirstName, testLastName, testEmail, testUsername, testPassword);
+    const newUser = new User(testFirstName, testLastName, testEmail, testUsername, testPassword, testSavedArticles);
     console.log(newUser);
     database.push(newUser);
 }
 
 class User{
-    constructor(firstName, lastName, email, username, password){
+    constructor(firstName, lastName, email, username, password, savedArticles){
         this.name = {
             firstName,
             lastName
@@ -20,5 +20,6 @@ class User{
         this.email = email;
         this.username = username;
         this.password = password;
+        this.savedArticles = savedArticles;
     }
 }
