80317b22ed184d1c8f4f6c239236e30af5751ecb
deybyr647 <deybyr647@gmail.com>
2020-08-11 21:45:22 -0400

    Additions and Tweaks to WIP map

--- recyclingCenters.js
+++ recyclingCenters.js
@@ -1,10 +1,20 @@
 let locationTrigger = document.querySelector('#queryLocation');
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGV5YnlyNjQ3IiwiYSI6ImNrZGt0OXZkdTByejQyd3Mya211YWh3enUifQ.ugRwxLxNtb5C204DEjS1WQ';
 
+let finalResults = [];
 let displayResults = (locationsArr) => {
+    for(let i = 0; i < locationsArr.length; i++){
+        let lon = locationsArr[i].geometry.coordinates[0];
+        let lat = locationsArr[i].geometry.coordinates[1];
 
+        let markerCoords = [lon, lat];
+
+        let marker = new mapboxgl.Marker();
+        marker.setLngLat(markerCoords);
+        finalResults.push(marker);
+
+    }
 }
-
 
 let getResults = (userCoords) => {
     let mapboxSrc = `https://api.mapbox.com/geocoding/v5/mapbox.places/recycling%20center.json?access_token=${mapboxgl.accessToken}&cachebuster=1597184952478&autocomplete=true&proximity=${userCoords.lon}%2C${userCoords.lat}&limit=10`;
@@ -18,8 +28,7 @@ let getResults = (userCoords) => {
             let locations = data.features;
             console.log('JSON', data);
             console.log('Locations', locations)
-
-            return locations;
+            displayResults(locations);
         })
 }
 
@@ -42,7 +51,15 @@ let getGeoLocation = () => {
                 zoom: 9
             });
 
+            let userLocation = new mapboxgl.Marker();
+            userLocation.setLngLat([userCoords.lon, userCoords.lat]);
+            userLocation.addTo(map);
+
             getResults(userCoords);
+
+            for(let x = 0; x < finalResults.length; x++){
+                finalResults[x].addTo(map);
+            }
         })
     }
 }
